a1ba6123a6f7bee068288509d11f4628ab4261bf
Andy Murph <murph@email.com>
2017-10-30 02:21:24 -0400

    added onclick event listener to 3 buttons.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button3 = (Button) findViewById(R.id.button_three_points);
+        Button button2 = (Button) findViewById(R.id.button_two_points);
+        Button button_free_throw = (Button) findViewById(R.id.button_free_throw);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayForTeamA(3);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayForTeamA(2);
+            }
+        });
+
+        button_free_throw.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayForTeamA(1);
+            }
+        });
+
     }
 
     /* Display the given score for Team A */
